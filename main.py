daa299ed7246d763d16b427ecd71f3f652007124
BioDigitalJazz <49828340+LaughClass@users.noreply.github.com>
2023-01-20 00:36:03 -0600

    Reformatted some things, added GUI, removed options menu func

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-import keyboard
+from tkinter import *
+
+root = Tk()
 
 alpha = ["[", "┤", "╕", "¿", "é", "ö", "₧", "º", "F", "ò", "?", "[", "╞", "┌", "a", "├", "j", "ô", "╣", "▌", "┐", "R",
          "(", "3", "╧", "Ö", "ú", "Æ", "P", "α", "7", "X", "<", "{", "S", "/", "x", "╡", "I", "ñ", "┬", "!", "╙", "Ü",
          "O", "┘", "í", "o", "A", ",", "¡", "ì", "½", "n", "0", "ß", "\'", "╗", "Å", "%", "╩", "E", "┴", "¬", "═", "╖",
-         "9",
-         "É", "+", "█", "ê", "æ", "╪", "╥", "Y", "w", "m", ";", ".", "▒", "¢", "@", "¼", "╫", "f", "▄", "h", "─", "░",
-         "C", "p", "║", "*", "¥", "=", "»", "i", "|", "#", "&", "~", "â", "1", "└", "y", "u", "s", "û", "╓", "q", "Q",
-         "Z", "å", "B", "▀", "-", "ẞ", "N", "e", "╛", "ä", "╝", "V", "d", "╦", "╟", "╔", ")", "ë", "r", "╤", "╬", "_",
-         "W",
-         "╒", "á", "ç", "z", "╠", "D", "Ñ", "M", "t", "5", "î", "╜", "Ç", "Ä", "│", "╚", "H", "▓", "]", "«", "ÿ", "ù",
-         "ü", "┼", "8", "G", "4", "c", "\"", "}", "ª", "k", "U", "g", "à", "⌐", "\\", "$", "`", ">", "£", "╨", "2", "K",
-         "l", "T", "6", "ï", "L", "è", "╢", "ó", "ƒ", "v", ":", "b", "^", "J", "]", " "]
+         "9", "É", "+", "█", "ê", "æ", "╪", "╥", "Y", "w", "m", ";", ".", "▒", "¢", "@", "¼", "╫", "f", "▄", "h",
+         "─", "░", "C", "p", "║", "*", "¥", "=", "»", "i", "|", "#", "&", "~", "â", "1", "└", "y", "u", "s", "û",
+         "╓", "q", "Q", "Z", "å", "B", "▀", "-", "ẞ", "N", "e", "╛", "ä", "╝", "V", "d", "╦", "╟", "╔", ")", "ë",
+         "r", "╤", "╬", "_", "W", "╒", "á", "ç", "z", "╠", "D", "Ñ", "M", "t", "5", "î", "╜", "Ç", "Ä", "│", "╚", "H",
+         "▓", "]", "«", "ÿ", "ù", "ü", "┼", "8", "G", "4", "c", "\"", "}", "ª", "k", "U", "g", "à", "⌐", "\\", "$", "`",
+         ">", "£", "╨", "2",
+         "K", "l", "T", "6", "ï", "L", "è", "╢", "ó", "ƒ", "v", ":", "b", "^", "J", "]", " "]
 
 shift = 0
 
 
 def encryption():
-    print("\n->Enter your message")
+    print("\nEnter your message")
     messagetoencrypt = input()
     encryptedmessage = "\n"
     for x in messagetoencrypt:
@@ -32,7 +33,7 @@ def encryption():
 
 
 def decryption():
-    print("\n->What is the message?")
+    print("\nWhat is the message?")
     messagetodecrypt = input()
     decryptedmessage = "\n"
     for x in messagetodecrypt:
@@ -45,23 +46,6 @@ def decryption():
         else:
             decryptedmessage += x
     print(decryptedmessage)
-
-
-def options():
-    option = 1
-    print("\n[C]hoose Date\n[E]ncrypt\n[D]ecrypt\n[Q]Quit")
-    while 1 == 1:
-        if keyboard.is_pressed("c"):
-            date()
-            options()
-        elif keyboard.is_pressed("e"):
-            encryption()
-            options()
-        elif keyboard.is_pressed("d"):
-            decryption()
-            options()
-        elif keyboard.is_pressed("q"):
-            quit()
 
 
 # capture date
@@ -99,4 +83,12 @@ def date():
     shift = shift - (len(alpha) * mathhold)
 
 
-options()
+datebutton = Button(root, text="Set Date", padx=50, pady=25, command=date)
+datebutton.grid(row=1, column=1)
+enbutton = Button(root, text="Encrypt", padx=50, pady=25, command=encryption)
+enbutton.grid(row=1, column=2)
+debutton = Button(root, text="Decrypt", padx=50, pady=25, command=decryption)
+debutton.grid(row=1, column=3)
+root.mainloop()
+
+quit()
